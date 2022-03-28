a209da43bbdfc0d868bc717230a7fc489bc6e67e
Ahmet Furkan <ahmetfunlu@outlook.com>
2022-03-29 00:05:28 +0300

    some little fixes - 404 handler & scripts

--- app.js
+++ app.js
@@ -21,14 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api', require('./routes/main_routes'));
 
-//#region 404
-app.get('*',(req,res)=>{
-    res.status(404).json({"message":"The Get route you wanted to acces is not exist"});
+app.use(function(req, res, next) {
+    res.status(404);
+    res.json({ error: 'Not found' });
 });
-app.post('*',(req,res)=>{
-    res.status(404).json({"message":"The Post route you wanted to acces is not exist"});
-});
-//#endregion
-
 
 app.listen(port, () => { console.log(`App running on port ${port}`) });
